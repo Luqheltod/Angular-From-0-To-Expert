48cb3b74444a54b63eec35c5872b416602b94549
German Sanchez <german.sanchezpastor@innotec.security>
2022-12-05 19:40:12 +0100

    changes

--- bases/src/app/app.component.ts
+++ bases/src/app/app.component.ts
@@ -6,13 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Contador';
 
-  numero : number = 0;
-  base: number = 5;
-
-
-  acumular(valor:number){
-    this.numero += valor;
-  }
 }
